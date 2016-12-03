caf4f8a6b0b3edc24c536990d49a1648cc751a9c
Kristopher Parrillo <kkparrillo@gmail.com>
2016-12-03 13:25:01 -0800

    adding span

--- main.js
+++ main.js
@@ -3,16 +3,24 @@ console.log("Good morning, Sunshine!");
 var input = document.querySelector('input');
 var listTodos = document.querySelector('.todos');
 
+
 var keyPress = function(event) {
   if (event.keyCode === 13) {
   console.log('keyCode', event.keyCode);
   var val = event.target.value;
-  var node = document.createElement('li');
-  var listItem = document.createTextNode(val);
-  listTodos.appendChild(node);
-  node.appendChild(listItem);
+  var li = document.createElement('li');
+  var textNode = document.createTextNode(val);
+  listTodos.appendChild(li);
+  li.appendChild(textNode);
   event.target.value = '';
-}
+  var qli = document.querySelector('li');
+  qli.classList.add("todo");
+  var liSpan = document.createElement('span')
+  liSpan.classList.add("remove");
+  document.querySelector('remove').textContent = 'X';
+
+  li.appendChild(liSpan);
+  }
 };
 
 
