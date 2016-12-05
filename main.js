2c06615ac8c68878ad62465db9e6e841bf5765be
Kristopher Parrillo <kkparrillo@gmail.com>
2016-12-04 20:31:02 -0800

    almost

--- main.js
+++ main.js
@@ -2,47 +2,57 @@ console.log("Good morning, Sunshine!");
 
 var input = document.querySelector('input');
 var listTodos = document.querySelector('.todos');
-
-
+var listItems = document.querySelector('.todo');
+// create a function to create a list item and add to list
 var handleKeyPress = function(event) {
+  // listen for Enter key
   if (event.keyCode === 13) {
   console.log('keyCode', event.keyCode);
+ // set the target value to a var val
   var val = event.target.value;
+  // creates a list item and saves to var li
   var li = document.createElement('li');
+  // create textNode and save to var textNode
   var textNode = document.createTextNode(val);
+  // append li and textNode to listTodos
   listTodos.appendChild(li);
   li.appendChild(textNode);
+  // create class name 'todo' for list item
   li.className = "todo";
+  // create element 'span'
   var liSpan = document.createElement('span')
+  // add class 'remove' to liSpan
   liSpan.classList.add('remove');
+  // append liSpan to li
   li.appendChild(liSpan);
-  liSpan.textContent = " X";
+  // adds text ' X' to liSpan
+  liSpan.textContent = ' X';
+  // clear the input value
   event.target.value = '';
+  li.addEventListener('click', handleCompleteOn);
+  liSpan.addEventListener('click', handleRemoveX)
   }
 };
 
-
+// create function to remove the X from the list item
 var handleRemoveX = function(event) {
-  console.log("event.target", event.target);
-  var x = document.querySelector('span');
-  x.textContent = '';
+console.log(this.parent);
+this.parent = ''
 };
 
 
 
+// create function to add class 'complete' to list item
 var handleCompleteOn = function(event) {
-  console.log('event.target', event.target);
-  var liComplete = event.target;
-  liComplete.classList.toggle("complete");
+  this.classList.toggle("complete");
 };
 
 
-
+// create event listener for keydown and click
 input.addEventListener('keydown', handleKeyPress);
 
-listTodos.addEventListener('click', handleRemoveX);
 
-listTodos.addEventListener('click', handleCompleteOn);
+
 
 
 
